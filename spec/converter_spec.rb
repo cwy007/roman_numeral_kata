4afbcf06ba3a726d496ec88902467880c48857c2
cwy007 <chanweiyan007@gmail.com>
2018-03-02 22:35:49 +0800

    Add spec for convertting 6 to "VI"

--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -8,9 +8,9 @@ RSpec.describe Converter do
   end
 
   describe "#decimal_to_roman" do
-    it { expect(decimal_to_roman(1)).to eq "I"   }
     it { expect(decimal_to_roman(2)).to eq "II"  }
     it { expect(decimal_to_roman(3)).to eq "III" }
     it { expect(decimal_to_roman(5)).to eq "V"   }
+    it { expect(decimal_to_roman(6)).to eq "VI"  }
   end
 end
