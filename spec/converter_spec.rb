d476abd771db7894a63a34fc76c4ae1ef318d3c6
cwy007 <chanweiyan007@gmail.com>
2018-03-02 22:47:38 +0800

    Add spec for convertting 20 to "XX"

--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -8,10 +8,12 @@ RSpec.describe Converter do
   end
 
   describe "#decimal_to_roman" do
+    it { expect(decimal_to_roman(1)).to eq "I"   }
     it { expect(decimal_to_roman(2)).to eq "II"  }
     it { expect(decimal_to_roman(3)).to eq "III" }
     it { expect(decimal_to_roman(5)).to eq "V"   }
     it { expect(decimal_to_roman(6)).to eq "VI"  }
     it { expect(decimal_to_roman(10)).to eq "X"  }
+    it { expect(decimal_to_roman(20)).to eq "XX" }
   end
 end
